bf3bec6b6e85b7f130e9ce350558beb475ab51b1
CyrilLepretre <cyril.lepretre@axa.fr>
2019-10-25 18:47:57 +0200

    Writer added

--- src/com/hemebiotech/analytics/ISymptomWriter.java
+++ src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -1,4 +1,16 @@
 package com.hemebiotech.analytics;
+import java.util.TreeMap;
 
+/**
+ * Write in a file the list of symptoms and how many times there are in the input file
+ *
+ *
+ */
 public interface ISymptomWriter {
+    /**
+     * @param TreeMap<String, Integer>, String
+     * @return true if the file was correctly written, false else
+     */
+
+    Boolean writeSymptoms (TreeMap<String, Integer> map, String filePath);
 }
--- src/com/hemebiotech/analytics/WriteSymptomFileFromData.java
+++ src/com/hemebiotech/analytics/WriteSymptomFileFromData.java
@@ -1,4 +1,38 @@
 package com.hemebiotech.analytics;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.io.FileWriter;
 
-public class WriteSymptomFileFromData {
+public class WriteSymptomFileFromData implements ISymptomWriter{
+
+    @Override
+    public Boolean writeSymptoms(TreeMap<String, Integer> symptomsMap, String filePath){
+        boolean result = true;
+        if (filePath != null) {
+            try {
+                if (symptomsMap != null){
+                    FileWriter writer = new FileWriter (filePath);
+                    Set<Map.Entry<String, Integer>> setsymptomsMap = symptomsMap.entrySet();
+                    Iterator<Map.Entry<String, Integer>> iterator = setsymptomsMap.iterator();
+                    while(iterator.hasNext()){
+                        Map.Entry<String, Integer> symptomLine = iterator.next();
+                        writer.write(symptomLine.getKey() + " : " + symptomLine.getValue() + "\n");
+                    }
+                    writer.close();
+                    result = true;
+                } else{
+                    System.out.println("Symptoms list empty");
+                    result = false;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
 }
